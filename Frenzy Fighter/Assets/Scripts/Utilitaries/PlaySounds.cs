1fab95590a175c46036b4f9911e8423570438a60
Thomas Bringer <45435618+ThomasBringer@users.noreply.github.com>
2024-04-29 19:16:56 +0200

    Footsteps sound

--- Frenzy Fighter/Assets/Scripts/Utilitaries/PlaySounds.cs
+++ Frenzy Fighter/Assets/Scripts/Utilitaries/PlaySounds.cs
@@ -8,9 +8,11 @@ public class PlaySounds : MonoBehaviour
     [SerializeField] AudioSource swingSource;
     [SerializeField] AudioSource swordSource;
     [SerializeField] AudioSource dieSource;
+    [SerializeField] AudioSource footSource;
 
     public void PlayHit() => hitSource.Play();
     public void PlaySwing() => swingSource.Play();
     public void PlaySword() => swordSource.Play();
     public void PlayDead() => dieSource.Play();
+    public void PlayFoot() => footSource.Play();
 }
